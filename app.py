403c89e83f47dab2d596f0184e5cf296d7b43b9e
DeekshaMarpadaga <113464102+DeekshaMarpadaga@users.noreply.github.com>
2023-02-11 21:42:50 +0000

    test

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4500) 
