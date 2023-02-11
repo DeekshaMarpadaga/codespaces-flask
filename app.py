80bd6d68d817e4e22db6ef07635de8cc38bf6d9e
DeekshaMarpadaga <113464102+DeekshaMarpadaga@users.noreply.github.com>
2023-02-11 22:18:49 +0000

    choices

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from forms import CourseForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+
+courses_list = [{
+    'title': 'Python 101',
+    'description': 'Learn Python basics',
+    'price': 34,
+    'available': True,
+    'level': 'Beginner'
+    }]
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = CourseForm()
+    return render_template('index.html', form=form)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4500) 
+    app.run(host='0.0.0.0', port=4500)
