ebb8744753b0ef549ca3cde8d5ff3402b8da2153
DeekshaMarpadaga <113464102+DeekshaMarpadaga@users.noreply.github.com>
2023-02-12 12:27:29 +0000

    finish

--- app.py
+++ app.py
@@ -1,22 +1,63 @@
 from flask import Flask, render_template, redirect, url_for
-from forms import CourseForm
+from forms import UserInput
+from forms import Nexti
+from forms import Budget
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+income=0
+living=0
+state=0
+transport=0
+insurance=0
+Meal_Plan=0
+loan=0
+interest=0
+total=0
 
 
-courses_list = [{
-    'title': 'Python 101',
-    'description': 'Learn Python basics',
-    'price': 34,
-    'available': True,
-    'level': 'Beginner'
-    }]
+
 
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    form = CourseForm()
+    form = UserInput()
+    if form.validate_on_submit():
+        if form.state.data=='In-state':
+            state=3,812
+        else:
+            state==17,936
+        if form.housing.data=='On Campus':
+            housing=9000
+        elif form.housing.data=='Off Campus':
+            housing=9000
+        else:
+            housing=0
+        if form.insurance.data=='No':
+            insurance=2704
+        else:
+            insurance=0
+        if form.meal_plan.data=='Yes':
+            Meal_Plan=3,858
+        else:
+            Meal_Plan=0
+
+       
+        return redirect(url_for('next'))
     return render_template('index.html', form=form)
+
+@app.route('/next/', methods=('GET', 'POST'))
+def next():
+    form=Nexti()
+    if form.validate_on_submit():
+        return redirect(url_for('spend')) 
+    return render_template('next.html', form=form)
+@app.route('/spend/', methods=('GET', 'POST'))
+def spend():
+    form=Budget()
+    if form.validate_on_submit():
+        return render_template('amounts.html')  
+    return render_template('amounts.html', form=form)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4500)
+    app.run(host='0.0.0.0', port=4600)
--- forms.py
+++ forms.py
@@ -1,16 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
-from wtforms.validators import InputRequired, Length
-class CourseForm(FlaskForm):
-
-    title = StringField('Title', validators=[InputRequired(),
-                                             Length(min=10, max=100)])
-    description = TextAreaField('Course Description',
-                                validators=[InputRequired(),
-                                            Length(max=200)])
-    price = IntegerField('Price', validators=[InputRequired()])
-    level = RadioField('Level',
-                       choices=['Beginner', 'Intermediate', 'Advanced'],
+from wtforms import (IntegerField, RadioField)
+from wtforms.validators import InputRequired
+class UserInput(FlaskForm):
+        state = RadioField('Are you in-state or out-of-state?',
+                       choices=['In-state', 'Out-of-State'],
                        validators=[InputRequired()])
-    available = BooleanField('Available', default='checked')
+
+        housing = RadioField('Where are you planning to live?',
+                       choices=['On Campus', 'Off Campus','At Home'],
+                       validators=[InputRequired()])
+        transport = RadioField('Do you have a car',
+                       choices=['Yes', 'No'],
+                       validators=[InputRequired()])
+        insurance = RadioField('Do you have insurance',
+                       choices=['Yes', 'No'],
+                       validators=[InputRequired()])
+        meal_plan = RadioField('Do you have a meal plan',
+                       choices=['Yes', 'No'],
+                       validators=[InputRequired()])
+class Nexti(FlaskForm):
+        income = IntegerField('Income(Can include financial aid, scholorships and parental support)', validators=[InputRequired()])
+        loan = RadioField('Do you have a loan? If yes what type of loan?',
+                       choices=['No Loan', 'Subsidized Loan','Private Loan','Unsubsidized Loan'],
+                       validators=[InputRequired()])
+        interest_rate=IntegerField('Loan Interest Rate,Enter 0 if no loan.',validators=[InputRequired()])
+        amount=IntegerField('Loan Amount, Enter 0 if no loan',validators=[InputRequired()])
+        years=IntegerField('How many years left in college',validators=[InputRequired()])
+        term=IntegerField('What is the loan term. Enter 0 if no loan',validators=[InputRequired()])
+
+class  Budget(FlaskForm):
+        entertainment = IntegerField('How much do you want to spend on entertainment', validators=[InputRequired()])
+
+        clothing=IntegerField('How much do you want to spend on clothing',validators=[InputRequired()])
+        food=IntegerField('How much do you want to spend on food',validators=[InputRequired()])
+        things=IntegerField('How much do you want to spend on other things?',validators=[InputRequired()])
+        invest=IntegerField('How much do you want to invest',validators=[InputRequired()])
+
+
+        
+        
